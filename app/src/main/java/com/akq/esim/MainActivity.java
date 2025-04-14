ed7e7996f24eb1d2fc0bb8d4e396949d3d140545
wencai <wencai>
2025-04-15 00:22:00 +0800

    1.0.8.2

--- app/src/main/java/com/akq/esim/MainActivity.java
+++ app/src/main/java/com/akq/esim/MainActivity.java
@@ -16,8 +16,11 @@ public class MainActivity extends Activity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+//        com.google.common.collect.Lists.transform()
 
-
+//        0>api.xml.class-parse: Warning BG8605 : The Java type 'com.google.common.collect.FluentIterable<T>' could not be found (are you missing a Java reference jar/aar or a Java binding library NuGet?)
+//        0>api.xml.class-parse: Warning BG8605 : The Java type 'com.google.common.collect.Lists.RandomAccessListWrapper<E>' could not be found (are you missing a Java reference jar/aar or a Java binding library NuGet?)
+//        com.google.common.collect.FluentIterable
     }
 
 }
