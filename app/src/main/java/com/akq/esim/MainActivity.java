ea690c9a9b9461e8418029d1164348718861ad37
康文才 <cai@qq.com>
2025-04-16 18:36:38 +0800

    清除room

--- app/src/main/java/com/akq/esim/MainActivity.java
+++ app/src/main/java/com/akq/esim/MainActivity.java
@@ -4,18 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
-import com.akq.esim.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
-     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+
+        setContentView(R.layout.activity_main);
 //        com.google.common.collect.Lists.transform()
 
 //        0>api.xml.class-parse: Warning BG8605 : The Java type 'com.google.common.collect.FluentIterable<T>' could not be found (are you missing a Java reference jar/aar or a Java binding library NuGet?)
